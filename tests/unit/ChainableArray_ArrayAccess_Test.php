91612ebfb1614bc61aff646aa10b7f06f31c73bb
Jean Claveau <jc@mediabong.com>
2018-11-19 23:47:33 +0100

    test array_key_exists for tracking

--- tests/unit/ChainableArray_ArrayAccess_Test.php
+++ tests/unit/ChainableArray_ArrayAccess_Test.php
@@ -15,7 +15,7 @@ class ChainableArray_ArrayAccess_Test extends \PHPUnit_Framework_TestCase
             'key_2' => 'zoubida',
         ])
         ;
-        
+
         $this->assertEquals('zoubida', $array['key_2']);
     }
 
@@ -26,7 +26,7 @@ class ChainableArray_ArrayAccess_Test extends \PHPUnit_Framework_TestCase
         $array = ChainableArray::from();
         $array[] = 'lalala';
         $array['my_key'] = 'plop';
-        
+
         $this->assertEquals('lalala', $array[0]);
         $this->assertEquals('plop', $array['my_key']);
     }
@@ -38,9 +38,9 @@ class ChainableArray_ArrayAccess_Test extends \PHPUnit_Framework_TestCase
         $array = ChainableArray::from();
         $array[] = 'lalala';
         $array['my_key'] = 'plop';
-        
+
         unset($array['my_key']);
-        
+
         try {
             $missing_row = $array['my_key'];
             $this->assertFalse(true, 'An exception should have been thrown here');
@@ -59,26 +59,39 @@ class ChainableArray_ArrayAccess_Test extends \PHPUnit_Framework_TestCase
             'plop',
         ]);
         $this->assertFalse( $array->hasDefaultRowValue() );
-            
+
         // scalar defaulkt row
         $array->setDefaultRow('lilili');
-            
+
         $this->assertEquals('lalala', $array[0]);
         $this->assertEquals('lilili', $array[2]);
         $this->assertEquals('lilili', $array['dfghjkl']);
-        
+
         $this->assertTrue( $array->hasDefaultRowValue() );
-        
+
         // default row generator
         $array->setDefaultRowGenerator(function($key) {
             return "'$key' is the best key ever";
         });
-        
+
         $this->assertEquals("':)' is the best key ever", $array[':)']);
-        
+
         // removed default row
         $array->unsetDefaultRow();
         $this->assertFalse( $array->hasDefaultRowValue() );
+    }
+
+    /**
+     */
+    public function test_array_key_exists()
+    {
+        $array = ChainableArray::from();
+        $array[] = 'lalala';
+        $array['my_key'] = 'plop';
+
+        // php bug https://stackoverflow.com/questions/1538124/php-array-key-exists-and-spl-arrayaccess-interface-not-compatible
+        $this->assertFalse( array_key_exists(0, $array) );
+        $this->assertFalse( array_key_exists('my_key', $array) );
     }
 
     /**/
